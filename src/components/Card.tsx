319911ae8c064d6cdd7786acbfaaec525a654cc3
Mikeal <mwiemszra@gmail.com>
2024-10-22 21:24:43 +0200

    🩹 Tags props bug fix

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -16,7 +16,7 @@ export default function Card({ id, title, imageUrl, year, description }: CardPro
           Read more
           <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
             viewBox="0 0 14 10">
-            <path stroke="currentColor" strokeLinecap="round" stroke-linejoin="round" stroke-width="2"
+            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
               d="M1 5h12m0 0L9 1m4 4L9 9" />
           </svg>
         </a>
